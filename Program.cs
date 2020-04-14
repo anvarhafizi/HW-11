13851848ece4cb226dd43cfa77e504dbeeaa1cc5
unknown <yourGitHub@example.com>
2020-04-14 22:58:50 +0500

    I create a class and methods (LastName,FirstName,MiddleName,Birthyear,Addres)

--- Program.cs
+++ Program.cs
@@ -9,5 +9,28 @@ namespace ConsoleApplication
            
         }   
     }
-   
+       class Student
+    {
+        public string LastName;
+        public string FirstName;
+        public string MiddleName;
+        public int Birthyear;
+        public string Addres;
+        public Student(String LastName, String FirstName, String MiddleName, int Birthyear, string Addres)
+        {
+            this.LastName=LastName;
+            this.FirstName=FirstName;
+            this.MiddleName=MiddleName;
+            this.Birthyear=Birthyear;
+            this.Addres=Addres;
+        }
+        public void info(int sill)
+        {
+            System.Console.WriteLine($"LastNames:{this.LastName}");
+            System.Console.WriteLine($"FirstName:{this.FirstName}");
+            System.Console.WriteLine($"MiddleName:{this.MiddleName}");
+            System.Console.WriteLine($"Birhtyear:{this.Birthyear}");
+            System.Console.WriteLine($"Addres:{this.Addres}");
+        }
+    }
 }
